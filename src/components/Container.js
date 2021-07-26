97088f973ec578282bb2718989386da0d5bff5a0
Peter Kang <peeterkang@gmail.com>
2021-07-25 18:39:52 -0700

    feat(pokedex): pagination
    - application renders twenty pokemon at a time, with a pagination component at the top to navigate between consecutive pokemon

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 import Card from './Card';
-
+import Pagination from '@material-ui/lab/Pagination';
 
 const useStyles = makeStyles({
 	container: {
@@ -13,10 +12,26 @@ const useStyles = makeStyles({
 		background: '#6082B6',
 		padding: '0.5rem',
 	},
+	pagination: {
+		'& .MuiPaginationItem-root': {
+			color: 'white',
+		},
+	},
+	paginationContainer: {
+		marginBottom: '2rem',
+	},
 });
 
 const Container = ({ pokeData }) => {
 	const classes = useStyles();
+	const pokemonPerPage = 20;
+	const [page, setPage] = useState(1);
+	const [numberOfPages] = useState(Math.ceil(pokeData.length / pokemonPerPage));
+
+	const handlePageChange = (event, value) => {
+		setPage(value);
+	};
+
 	return (
 		<Grid
 			className={classes.container}
@@ -26,8 +41,46 @@ const Container = ({ pokeData }) => {
 			alignItems='center'
 			data-cy='container'
 		>
-			{pokeData && pokeData.map((poke) => <Card pokemon={poke} />)}
-
+			<Grid
+				className={classes.paginationContainer}
+				item
+				container
+				direction='row'
+				justifyContent='center'
+				alignItems='center'
+				xs={12}
+			>
+				<Grid item>
+					<Pagination
+						count={numberOfPages}
+						page={page}
+						onChange={handlePageChange}
+						defaultPage={1}
+						// color='primary'
+						size='large'
+						showFirstButton
+						showLastButton
+						classes={{ ul: classes.pagination }}
+					/>
+				</Grid>
+			</Grid>
+			{pokeData &&
+				pokeData
+					.slice((page - 1) * pokemonPerPage, page * pokemonPerPage)
+					.map((poke) => <Card pokemon={poke} />)}
+			{/* <span> */}
+			{/* <Pagination
+					count={numberOfPages}
+					page={page}
+					onChange={handlePageChange}
+					defaultPage={1}
+					// color='primary'
+					// size='large'
+					showFirstButton
+					showLastButton
+					// classes={{ ul: classes.paginator }}
+				/> */}
+			{/* </span> */}
 		</Grid>
 	);
 };
